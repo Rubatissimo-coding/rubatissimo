8d388a3e46f20c03ad507bd0de7d02e3a22b14fd
Rubatissimo-coding <rubatissimo.coding@gmail.com>
2022-07-30 18:02:34 +0200

    public path again (last time?)

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/rubatissimo/dist" : "/dist"
+  publicPath: process.env.NODE_ENV === "production" ? "/rubatissimo/" : "/"
 })
