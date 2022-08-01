244e64dfd4e419edec89ab1e63e2bda4a6884d85
Rubatissimo-coding <rubatissimo.coding@gmail.com>
2022-08-01 11:10:27 +0200

    created 404 page

---
+++ src/assets/scripts/texts/404/index.js
@@ -0,0 +1,17 @@
+export const error404 = {
+  subtitle: {
+    en: "Oops, what you searched for was not found...",
+    fr: "Oups, ce que vous avez recherché n'a pas été trouvé...",
+    ru: "Ой, что вы искали не найдено..."
+  },
+  text: {
+    en: "Got lost while searching for the end of Schubert's unfinished symphony? Sorry but it's not here...",
+    fr: "Perdu en recherchant la fin de la symphonie inachevée de Schubert ? Désolé mais elle n'est pas ici...",
+    ru: "Вы потерялись в поиске неоконченной симфонии Шуберта? К сожалению, она не найдена..."
+  },
+  return: {
+    en: "Return home",
+    fr: "Retourner à l'accueil",
+    ru: "Вернуться к главной"
+  }
+}
---
+++ src/views/404.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-card class="black d-flex justify-center align-center flex-column ma-6">
+      <v-card-title class="text-h2">
+        404
+      </v-card-title>
+      <v-card-subtitle class="text-h4">
+        {{ texts.error404.subtitle[$lang] }}
+      </v-card-subtitle>
+      <v-card-text class="text-h5 text-center">
+        {{ texts.error404.text[$lang] }}
+      </v-card-text>
+      <v-img src="@/assets/images/schubert.png" width="300"></v-img>
+      <v-card-text class="text-h5 text-center">
+        <router-link class="white--text" :to="{ name: 'home' }">{{ texts.error404.return[$lang].toLowerCase() }}</router-link>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import * as texts from "@/assets/scripts/texts"
+
+export default {
+  data() {
+    return {
+      texts
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
